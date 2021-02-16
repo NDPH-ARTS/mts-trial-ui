d2a604fb95535f581e11451488a7a2156beea397
Kate Sant <katesan@ndph.ox.ac.uk>
2021-02-16 12:46:56 +0000

    [ARTS-477] HTTP request to /profile endpoint on localhost

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import {Profile} from "./Profile";
 import { Observable, of } from 'rxjs';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private profileUrl = 'http://localhost:8081/practitioner/profile'; // temp. Inject this in task 549
+
+  constructor(private http: HttpClient) { }
 
   getProfile(): Observable <Profile> {
-    return of (this.mockProfile); // mock for now
+    return this.http.get<Profile>(this.profileUrl)
   }
+
 
   private mockProfile : Profile = {
     id : 'mock-id-123',
