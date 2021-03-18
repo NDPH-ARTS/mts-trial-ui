0abc310a8d949209d1be98560f07489948237b05
Mark Phipps <mark.phipps0@gmail.com>
2021-03-18 08:57:44 +0000

    ARTS-48 Add comment around gatewayUrl use

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,10 @@ export const environment = {
   issuer: 'https://login.microsoftonline.com/5d23383f-2acb-448e-8353-4b4573b82276/v2.0',
   clientId: 'f352ce15-0142-4dfa-8e18-801ee6391557',
   trialName: 'mts-trial-ui',
-  // either specify the gateway root or give the URLs  for the services individually
+
+  //  This URL is used during the PR UI test run; ensure that it points to
+  //  a working environment during the PR process.
+  //  This URL is overridden in the Azure deployment process
   gatewayUrl: 'https://as-liza48a-sc-gateway-dev.azurewebsites.net/api',
 };
 /*
