343518a434f4aee8e9ef38abbc95d9b1d09bbcb3
Mark Phipps <mark.phipps0@gmail.com>
2021-03-22 08:22:08 +0000

    ARTS-190 Improve test coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { AppComponent } from './app.component';
 import { MockAuthenticationService } from './services/oauth2-authentication.mock';
@@ -8,23 +8,18 @@ import { AuthenticationService } from './services/oauth2-authentication.service'
 
 describe('AppComponent', () => {
   const mockAuthenticationService = new MockAuthenticationService();
-  const mockTranslateService = {
-    setDefaultLang: (s: string) => { },
-    use: (s: string) => of(s)
-  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        TranslateModule.forRoot()
       ],
       declarations: [
         AppComponent
       ],
       providers: [
         { provide: AuthenticationService, useValue: mockAuthenticationService },
-        { provide: TranslateService, useValue: mockTranslateService },
-
       ]
     }).compileComponents();
   });
@@ -50,5 +45,35 @@ describe('AppComponent', () => {
     app.logout();
 
     expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call the translation service to set the default locale', () => {
+    const translateService = TestBed.inject(TranslateService);
+    const defaultLangSpy = spyOn(translateService, 'setDefaultLang');
+    const useSpy = spyOn(translateService, 'use');
+    TestBed.createComponent(AppComponent);
+
+    expect(defaultLangSpy).toHaveBeenCalledWith('en-gb');
+    expect(useSpy).toHaveBeenCalledWith('en-gb');
+  });
+
+  it('should set the current locale to the user\'s stored preference if present', () => {
+    const translateService = TestBed.inject(TranslateService);
+    const useSpy = spyOn(translateService, 'use');
+    spyOn(window.localStorage, 'getItem').and.callFake((s: string) => 'fr-fr');
+    TestBed.createComponent(AppComponent);
+
+    expect(useSpy).toHaveBeenCalledWith('fr-fr');
+  });
+
+  it('should call the translateService when the user selects a locale', () => {
+    const translateService = TestBed.inject(TranslateService);
+    const useSpy = spyOn(translateService, 'use');
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.onLocaleChange('de-de');
+
+    expect(useSpy).toHaveBeenCalledWith('de-de');
   });
 });
