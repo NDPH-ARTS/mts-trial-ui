3a98914be53aa3378d4dd402499f17ce1f05ab36
Mark Phipps <mark.phipps0@gmail.com>
2021-03-26 15:16:05 +0000

    ARTS-752 Introduce window size param

--- ui-tests/conf/wdio.conf.ci.js
+++ ui-tests/conf/wdio.conf.ci.js
@@ -20,7 +20,6 @@ exports.config = {
         featureFilePath
     ],
 
-    logLevel: 'debug',
     bail: 0,
 
     //baseUrl: 'http:',
@@ -68,12 +67,25 @@ exports.config = {
     },
 
     capabilities: [
+        // {
+        //     browserName: 'firefox',
+        //     'moz:firefoxOptions': {
+        //       // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+        //       args: ['-headless']
+        //     },
+        //     // If outputDir is provided WebdriverIO can capture driver session logs
+        //     // it is possible to configure which logTypes to exclude.
+        //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        //     excludeDriverLogs: ['bugreport', 'server'],
+        //     //
+        //     // Parameter to ignore some or all Puppeteer default arguments
+        //     // ignoreDefaultArgs: ['-foreground'], // set value to true to ignore all default arguments
+        // },        
         {
             browserName: 'chrome',
             'goog:chromeOptions': {
-                args: ['--headless', '--disable-gpu', '--no-sandbox'],
+                args: ['--headless','--no-sandbox', '--window-size=1920,1080'],
             }
-
         }
     ],
     sync: true,
