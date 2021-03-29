bc10064085d5d890c6906a3ce7ef7193d9ef97b4
Mark Phipps <mark.phipps0@gmail.com>
2021-03-29 09:39:55 +0100

    ARTS-752 cleanup

--- ui-tests/conf/wdio.conf.ci.js
+++ ui-tests/conf/wdio.conf.ci.js
@@ -67,24 +67,10 @@ exports.config = {
     },
 
     capabilities: [
-        // {
-        //     browserName: 'firefox',
-        //     'moz:firefoxOptions': {
-        //       // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
-        //       args: ['-headless']
-        //     },
-        //     // If outputDir is provided WebdriverIO can capture driver session logs
-        //     // it is possible to configure which logTypes to exclude.
-        //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        //     excludeDriverLogs: ['bugreport', 'server'],
-        //     //
-        //     // Parameter to ignore some or all Puppeteer default arguments
-        //     // ignoreDefaultArgs: ['-foreground'], // set value to true to ignore all default arguments
-        // },        
         {
             browserName: 'chrome',
             'goog:chromeOptions': {
-                args: ['--headless','--no-sandbox', '--window-size=1920,1080'],
+                 args: ['--headless','--no-sandbox', '--window-size=1920,1080'],
             }
         }
     ],
