409b50e19beaa495af22788276ce0b5d8fdf7270
Darren Smithson <darrengsmithson@hotmail.com>
2021-04-16 12:35:19 +0100

    Update adminSitesPage.page.js

--- ui-tests/pages/adminSitesPage.page.js
+++ ui-tests/pages/adminSitesPage.page.js
@@ -5,8 +5,8 @@ class adminSitesPage {
     get myadminSites() { return $('//a[@href="/admin-sites"]') }
     get lastUpdated() { return $('//div//table//tr//th[4]') }
     get modal() { return $('//tr//td') }
-    get siteDescription() { return $('//div//span[text()="Site Decsription"]') }
-    get siteStatus() { return $('//div//span[text()="Site Status"]') }
+    get siteDescription() { return $('//div//span[text()="Description"]') }
+    get siteStatus() { return $('//div//span[text()="Status"]') }
 
     adminSitesTab() {
         this.myadminSites.click();
