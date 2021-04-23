a36a0bd01e118c2fce11e8ca51d0caabb4ce5d25
Sameera Purini <31847267+sameerapurini@users.noreply.github.com>
2021-04-23 18:36:27 +0100

    [ARTS-913] - Rename ui tests to e2e tests (#40)

--- e2e-tests/conf/wdio.conf.ci.js
+++ e2e-tests/conf/wdio.conf.ci.js
@@ -6,8 +6,8 @@ const { removeSync } = require('fs-extra');
 // The below module is used for cucumber html report generation
 const reporter = require('cucumber-html-reporter');
 const currentTime = new Date().toJSON().replace(/:/g, "-");
-const sourceSpecDirectory = `ui-tests/features`;
-const jsonTmpDirectory = `ui-tests/reports/json/tmp/`;
+const sourceSpecDirectory = `e2e-tests/features`;
+const jsonTmpDirectory = `e2e-tests/reports/json/tmp/`;
 
 
 let featureFilePath = `${sourceSpecDirectory}/*.feature`;
@@ -39,7 +39,7 @@ exports.config = {
     reporters: [
         ['junit', {
             outputDir: './test-results',
-            outputFileFormat: function(options) { // optional
+            outputFileFormat: function (options) { // optional
                 return `wdio-results-${options.cid}.xml`
             }
         }],
@@ -51,7 +51,7 @@ exports.config = {
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
-        require: ['ui-tests/steps/*.js'],        // <string[]> (file/dir) require files before executing features
+        require: ['e2e-tests/steps/*.js'],        // <string[]> (file/dir) require files before executing features
         backtrace: false,   // <boolean> show full backtrace for errors
         requireModule: [],  // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
         dryRun: false,      // <boolean> invoke formatters without executing steps
@@ -69,28 +69,28 @@ exports.config = {
 
     capabilities: [
         // {
-            // browserName: 'chrome',
-            // 'goog:chromeOptions': {
-            //     args: ['--headless', '--disable-gpu', '--no-sandbox'],
-            // }
-            {
-                // maxInstances can get overwritten per capability. So if you have an in house Selenium
-                // grid with only 5 firefox instance available you can make sure that not more than
-                // 5 instance gets started at a time.
-                maxInstances: 5,
-                browserName: 'firefox',
-                'moz:firefoxOptions': {
-                  // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
-                  args: ['-headless']
-                },
-                // If outputDir is provided WebdriverIO can capture driver session logs
-                // it is possible to configure which logTypes to exclude.
-                // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-                excludeDriverLogs: ['bugreport', 'server'],
-                //
-                // Parameter to ignore some or all Puppeteer default arguments
-                // ignoreDefaultArgs: ['-foreground'], // set value to true to ignore all default arguments
+        // browserName: 'chrome',
+        // 'goog:chromeOptions': {
+        //     args: ['--headless', '--disable-gpu', '--no-sandbox'],
+        // }
+        {
+            // maxInstances can get overwritten per capability. So if you have an in house Selenium
+            // grid with only 5 firefox instance available you can make sure that not more than
+            // 5 instance gets started at a time.
+            maxInstances: 5,
+            browserName: 'firefox',
+            'moz:firefoxOptions': {
+                // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+                args: ['-headless']
             },
+            // If outputDir is provided WebdriverIO can capture driver session logs
+            // it is possible to configure which logTypes to exclude.
+            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+            excludeDriverLogs: ['bugreport', 'server'],
+            //
+            // Parameter to ignore some or all Puppeteer default arguments
+            // ignoreDefaultArgs: ['-foreground'], // set value to true to ignore all default arguments
+        },
     ],
     sync: true,
     logLevel: 'error',
