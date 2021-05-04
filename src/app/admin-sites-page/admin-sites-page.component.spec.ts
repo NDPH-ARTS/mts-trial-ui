18c7564f1e605aed7001844d6f06e0d2e06b415a
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-05-04 18:14:13 +0100

    Revert "[ARTS-915]"
    This reverts commit 3939831cc63ba48193d3f1d005cffb465272c242.

--- src/app/admin-sites-page/admin-sites-page.component.spec.ts
+++ src/app/admin-sites-page/admin-sites-page.component.spec.ts
@@ -36,16 +36,18 @@ describe('AdminSitesPageComponent', () => {
   it('should fetch admin sites on init', fakeAsync(() => {
 
     const site: Site =
-      {name: '', alias: '', siteId: '123', parentSiteId: '456', parentSiteName: 'parent', siteType: '', description: '',
+      {name: '', alias: '', siteId: '123', parentSiteId: '456', parentSiteName: '', siteType: '', description: '',
         lastUpdated: '', status: ''};
     const sites: Site[] = [site];
 
     const adminSiteSpy = spyOn(siteService, 'getSitesByRole').withArgs('admin').and.returnValue(of(sites));
+    const siteWithIdSpy = spyOn(siteService, 'getSite').withArgs(site.parentSiteId).and.returnValue(of({} as Site));
+
     component.ngOnInit();
     tick();
     fixture.detectChanges();
 
     expect(adminSiteSpy).toHaveBeenCalled();
-
+    expect(siteWithIdSpy).toHaveBeenCalled();
   }));
 });
