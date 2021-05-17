afb176ad8794760f070e92c27e115d695a4f304e
Sameera Purini <31847267+sameerapurini@users.noreply.github.com>
2021-05-17 14:06:06 +0100

    [ARTS-925] - UI tests using Cypress (#48)
    * UI tests using Cypress
    
    * updated the script in package json file

---
+++ cypress/plugins/index.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+// `on` is used to hook into various events Cypress emits
+// `config` is the resolved Cypress config
+const path = require('path');
+module.exports = (on, config) => {
+  on('before:browser:launch', (browser = {}, args) => {
+    console.log(config, browser, args);
+    if (browser.name === 'chrome') {
+      const ignoreXFrameHeadersExtension = path.join(__dirname, '../extensions/ignore-x-frame-headers');
+      args.push(args.push(`--load-extension=${ignoreXFrameHeadersExtension}`));
+      args.push("--disable-features=CrossSiteDocumentBlockingIfIsolating,CrossSiteDocumentBlockingAlways,IsolateOrigins,site-per-process");
+    }
+    return args;
+  });
+};
+
